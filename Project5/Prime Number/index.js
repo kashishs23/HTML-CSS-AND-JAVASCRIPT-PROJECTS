e2a8b61d8b7d0b45a066ce6516e2a7ace6daf4bc
Kashish Saini <kashishsaini83078@gmail.com>
2025-02-28 08:52:18 +0530

    Add files via upload

---
+++ Project5/Prime Number/index.js
@@ -0,0 +1,17 @@
+function f(){
+var i,x,flag=0;
+x=parseInt(window. prompt("enter the no"));
+if(x%2==0)
+{
+flag=1;
+}
+
+if (flag==1)
+{
+document.write("no prime no. "+x);
+}
+else{
+document.write("yes  prime no. "+x);
+}
+
+}
