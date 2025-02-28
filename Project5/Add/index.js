cf111730590ed533a805f4fcaf57d233f6c03e87
Kashish Saini <kashishsaini83078@gmail.com>
2025-02-28 08:26:12 +0530

    Add files via upload

---
+++ Project5/Add/index.js
@@ -0,0 +1,32 @@
+function add()
+{
+var a,b,c;
+a=parseInt(window. prompt("enter the no"));
+b=parseInt(window. prompt("enter the no"));
+c=a+b;
+document. write("your output is ="+c);
+}
+function sub()
+{
+var a,b,c;
+a=parseInt(window. prompt("enter the no"));
+b=parseInt(window. prompt("enter the no"));
+c=a-b;
+document. write("your output is ="+c);
+}
+function multi()
+{
+var a,b,c;
+a=parseInt(window. prompt("enter the no"));
+b=parseInt(window. prompt("enter the no"));
+c=a*b;
+document. write("your output is ="+c);
+}
+function divd()
+{
+var a,b,c;
+a=parseInt(window. prompt("enter the no"));
+b=parseInt(window. prompt("enter the no"));
+c=a/b;
+document. write("your output is ="+c);
+}
